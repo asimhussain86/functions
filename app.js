4ec3361909979890ce21bd8138969d6dd42f9391
Rana Asim Hussain <110455935+asimhussain86@users.noreply.github.com>
2023-06-22 11:44:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+// let x = 10;
+// let y = 11;
+// let sum = x + y;
+// console.log(sum);
+
+//Function Declaration
+
+// function sum(x, y) {
+//   let add = x + y;
+//   console.log("The sum of x and y is " + add);
+// }
+// // Function call
+// sum(10, 11);
+// sum(10, 12);
+// sum(1012, 12);
+// sum(1012, 12235423);
+
+// expression
+
+// sum =x+y;
+
+// const x = function () {
+//   console.log("Function expression");
+// };
+
+// x();
+
+s = (x, y) => {
+  let sum = x + y;
+  console.log(sum);
+};
+s(10, 11);
